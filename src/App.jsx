59cd9c86f40b7323cbf14a8ef76d4874a2808d6b
FMichel5173 <fmichel81@sfr.fr>
2023-10-28 05:50:36 +0200

    About component with style and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import ContactInfo from "./components/ContactInfo";
 import Team from "./components/Team";
+import About from "./components/About";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/contact' element={<ContactInfo />} />
           <Route path='/team' element={<Team />} />
+          <Route path='/about' element={<About />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import "../styles/About.css";
+
+function About() {
+  return (
+      <div className="about-container">
+          <h2>Qui sommes-nous ?</h2>
+          <h3>Notre Histoire</h3>
+          <p>
+          Le conseil local FCPE de Tinqueux a vu le jour il y a de nombreuses années, grâce à la passion et à l'engagement de quelques parents soucieux du bien-être et de l'éducation de nos enfants. Cependant, malgré une détermination sans faille, le conseil est resté en sommeil pendant un certain temps. Il fallait du temps, des ressources et, surtout, une nouvelle équipe dynamique pour insuffler une nouvelle vie à cette noble cause.
+          </p>
+          <p>
+          En janvier 2023, nous avons décidé de faire renaître ce conseil local. Avec une équipe dévouée et pleine d'énergie, nous nous sommes lancés dans plusieurs initiatives, dont la plus marquante a été l'opération Fournitures scolaires en avril 2023, assurant ainsi que nos élèves aient les outils nécessaires pour une rentrée scolaire réussie en août 2023.
+          </p>
+          <h3>Notre Philosophie</h3>
+          <p>
+          Nous croyons fermement qu'une approche collaborative est la clé pour apporter des changements positifs dans l'éducation. Ainsi, nous avons choisi de nous positionner comme partenaires des équipes enseignantes, de la municipalité, mais aussi comme facilitateurs et accompagnateurs pour nos familles et élèves. Nous nous efforçons de construire des ponts, de faciliter les communications et d'être une force de soutien pour tous.
+          </p>
+          <h3>Pourquoi la FCPE</h3>
+          <p>
+          La FCPE nous offre la structure et le soutien dont nous avons besoin pour avoir un impact réel et durable. Tout en nous permettant d'agir localement, avec une grande liberté dans nos actions et objectifs, elle nous donne également accès à un réseau plus large. Ce soutien est essentiel pour nous permettre d'intervenir efficacement auprès des différentes administrations et commissions, garantissant ainsi que nous puissions toujours agir dans l'intérêt de nos élèves, familles et établissements scolaires.
+          </p>
+          <h3>Un appel à l'Action</h3>
+          <p>
+          Peu importe que vous soyez adhérent de la FCPE ou non, nous vous invitons à nous rejoindre. Nous croyons que, ensemble, nous pouvons faire une différence. Car au final, nous partageons tous le même objectif : veiller au bien-être et à la réussite de nos enfants. Rejoignez-nous dans cette mission et contribuons ensemble à un avenir meilleur pour nos enfants.
+          </p>
+      </div>
+  );
+}
+
+export default About;
