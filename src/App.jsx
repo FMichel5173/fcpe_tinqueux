f74708c57dddbc1901043537d45c5eb733d768b1
FMichel5173 <fmichel81@sfr.fr>
2023-10-29 22:38:16 +0100

    Layout of welcome text addition of basename in App

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/fcpe_tinqueux">
       <div className='app-container'>
         <Header />
         <Routes>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,12 +9,18 @@ function Home() {
       </h1>
       <p>
         Chers parents, bienvenue sur le site du Conseil Local de la FCPE de
-        Tinqueux. Notre association est fière de rassembler des parents engagés
-        et déterminés à œuvrer pour le bien-être et la réussite scolaire de nos
-        enfants. Ici, vous trouverez toutes les informations concernant nos
-        actions, nos événements à venir, ainsi que les différentes manières de
-        nous rejoindre et de participer activement à la vie scolaire de notre
-        commune. Ensemble, faisons de l'éducation une priorité !
+        Tinqueux.
+      </p>
+      <p>
+        Notre association est fière de rassembler des parents engagés et
+        déterminés à œuvrer pour le bien-être et la réussite scolaire de nos
+        enfants.
+      </p>
+      <p>
+        Ici, vous trouverez toutes les informations concernant nos actions, nos
+        événements à venir, ainsi que les différentes manières de nous rejoindre
+        et de participer activement à la vie scolaire de notre commune.Ensemble,
+        faisons de l'éducation une priorité !
       </p>
     </div>
   );
